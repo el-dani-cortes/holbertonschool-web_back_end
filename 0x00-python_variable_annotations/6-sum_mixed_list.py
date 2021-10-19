dd36b322d98c340f665a7b75f793cb0cdcf7cde1
el-dani-cortes <danicor83@gmail.com>
2021-10-18 19:52:26 -0500

    Fixed function description

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """Sum of float numbers from a list"""
+    """Sum of float and integers numbers from a list"""
     sum = 0
     for number in mxd_lst:
         sum += number
